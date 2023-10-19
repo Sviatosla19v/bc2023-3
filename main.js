e848efa873b2651599fd7a8bdcd917908d5dc7b9
Sviatoslav Kozak <sviatoslav.kozakk@mail.com>
2023-10-19 20:27:30 +0300

    first

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 const fs = require("fs");
 fs.readFile("data.json", (err, data) => {
     if (err === null) {
-        const jsonData = JSON.parse(data);
-        const filteredData = jsonData.filter(enty => enty.parent === "BS3_BanksLiab");
-        const outputText = filteredData.map(enty => `${entry.txten}:${entry.value}`).join('\n');
-        console.log(data.toString());
-    } else {
-        console.log(err); 
-    }
-});   
-
+        let jsonData = JSON.parse(data);
+        const filteredData = jsonData.filter(entry => entry.parent === "BS3_BanksLiab");
+        const outputText = filteredData.map(entry => `${entry.txten}:${entry.value}`).join('\n');
+        
 fs.writeFile('output.txt', outputText, (err) => {
     if (err === null) {
         console.log("The file has been!");
     }  else {
         console.log(err);
     }
-});   
- 
+}); 
+    } else {
+       console.log(err); 
+    }
+});
